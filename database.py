a61b662d61100f3712f07ca93c4f869b3478019f
Jack Chan <jackccy@gmail.com>
2023-05-28 14:41:24 +0800

    remove dotenv completely

--- database.py
+++ database.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import os
-from dotenv import load_dotenv
 
 print('Connecting to database...')
 
