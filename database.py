845fe5a7d8b31665b91d94bda0ca8b5b013df2a7
Jack Chan <jackccy@gmail.com>
2023-05-28 09:19:21 +0800

    support postgreSQL

--- database.py
+++ database.py
@@ -2,10 +2,25 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 current_dir = os.path.dirname(__file__)
 
-engine = create_engine('sqlite:////{}/luckydraw.db'.format(current_dir), convert_unicode=True)
+# for sqlite connection
+# engine = create_engine('sqlite:////{}/luckydraw.db'.format(current_dir), convert_unicode=True)
+
+# for postgresql connection
+engine = create_engine(os.environ.get("POSTGRES_URL"), convert_unicode=True)
+
+#
+# engine = conn = psycopg2.connect(
+#     host=os.environ.get("POSTGRES_HOST"),
+#     database=os.environ.get("POSTGRES_DATABASE"),
+#     user=os.environ.get("POSTGRES_USER"),
+#     password=os.environ.get("POSTGRES_PASSWORD"))
+###
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
